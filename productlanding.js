50b1c993ec64057fc98b1631f413f6a500b63e0a
Petra Rabai <petra.rabai@icloud.com>
2018-09-29 12:06:23 +0200

    Modify HMTL and CSS file

--- productlanding.js
+++ productlanding.js
@@ -1,4 +1,4 @@
-/*This is a Free Code Camp project - Survey Form page - JS file*/
+/*This is a Free Code Camp project - Product Landing page - JS file*/
 var input = document.querySelectorAll("input");
 for (i=0; i<input.length; i++) {
     input[i].setAttribute('size',input[i].getAttribute('placeholder').length);
